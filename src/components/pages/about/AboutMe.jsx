2d59cb2b3ce5cbe80c130f3a13d84fda7c091582
AnaBt <anajuliaccomp@gmail.com>
2025-09-24 17:44:58 -0300

    djfnj

--- src/components/pages/about/AboutMe.jsx
+++ src/components/pages/about/AboutMe.jsx
@@ -11,30 +11,46 @@ export default function AboutMe() {
 
     <div className = "aboutContent">
       <div className = "aboutleft">
+
     <div className = "whoiam">
       <h2>Who I am</h2>
       <p>I'm Ana, a Full Stack Developer with a passion for creating innovative web applications and exploring blockchain technologies. I'm currently pursuing a Master's in Computer Science at Northeastern University, where I continue to expand my knowledge and skills.<br></br><br></br>
     With expertise in Next.js, React, and various blockchain technologies, I enjoy building scalable and user-friendly applications that solve real-world problems. My background in computer science has given me a strong foundation in algorithms, data structures, and system design, which I apply to create efficient and maintainable code.<br></br><br></br>
     I'm particularly interested in the intersection of web development and blockchain technology, where I've developed several projects that leverage the power of decentralized systems to create secure and transparent applications.</p>
     </div>
+
     <div className = "journey">
       <h2>Journey</h2>
       <Educard
        nome = "Bachelor of Science in Computer Science"
        uni = "UFSC" 
        data = "January 2025 - May 2027"
-       lugar = "Florianopolis - Brasil"/>
-      <Educard />
+       lugar = "Florianopolis - Brasil" />
+  
       <Educard
        nome = "Bachelor of Science in Computer Science"
        uni = "UFSC" 
        data = "January 2025 - May 2027"
-       lugar = "Florianopolis - Brasil"/>
-      <Educard />
+       lugar = "Florianopolis - Brasil" />
     </div>
     </div>
-    <div className = "aboutright">
 
+
+    <div className = "aboutright">
+      
+      <section className = "skills">
+        <h2>Skills</h2>
+        <h3>Languages</h3>
+        <ul>
+              {[
+           "java" , "python" , "c++" , "R" , "Go" , "TypeCript" , 
+           "java" , "python" , "c++" , "R" , "Go" , "TypeCript" , 
+           "java" , "python"
+        ].map((item) => (
+                <li className = "itemSkill">{item}</li>
+              ))}
+              </ul>
+      </section>
     </div>
     
     </div>
--- src/components/pages/about/educard.jsx
+++ src/components/pages/about/educard.jsx
@@ -4,11 +4,11 @@ export default function educard({nome,uni,data,lugar}){
     return(
         <div className="educard">
         <div className = "left">
-        <p>{nome}</p>
-        <p>{uni}</p>
-        <p>{data}</p></div>
+        <p className = "nome">{nome}</p>
+        <p className = "uni"> {uni}</p>
+        <p className = "data">{data}</p></div>
         <div className = "right">
-        <p>{lugar}</p>
+        <p  className = "lugar">{lugar}</p>
         </div>
         </div>
     );
